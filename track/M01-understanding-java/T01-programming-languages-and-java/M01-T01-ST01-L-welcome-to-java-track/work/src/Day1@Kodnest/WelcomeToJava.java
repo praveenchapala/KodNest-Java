9db4676ca5c467a18eb95676e76513ef555959df
chapalapraveen <praveenpraveensimhadri@gmail.com>
2026-07-31 15:02:48 +0530

    Understood Programming vs Programming languages

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Day1@Kodnest/WelcomeToJava.java
@@ -0,0 +1,8 @@
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java Track");
+        System.out.println("I Already know how to build the programming logic");
+        System.out.println("Now Iam learning how to express the logic in java");
+        System.out.println("Looking forward to become a great developer");
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++
@@ -1,5 +0,0 @@
-public class WelcomeToJava {
-    public static void main(String[] args) {
-       //Write your code here
-    }
-}
