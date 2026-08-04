6b58a01405b56923c70bbf599a01f94bcd3c1015
chapalapraveen <praveenpraveensimhadri@gmail.com>
2026-08-04 11:34:07 +0530

    Add Main3.java

---
+++ track/M02/T01/ST01/P01/Main3.java
@@ -0,0 +1,11 @@
+public class Main3{
+    public static void main(String args[]){
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("saturday");
+        System.out.println("Sunday");
+    }
+}
